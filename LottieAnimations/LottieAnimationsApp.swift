e10c44e88e089b7183171d26d45ec38511fd3a6d
Mohit Tomer <mstomer31@gmail.com>
2023-12-26 22:12:54 +0530

    Initial Commit

---
+++ LottieAnimations/LottieAnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsApp.swift
+//  LottieAnimations
+//
+//  Created by Mohit Tomer on 26/12/23.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
